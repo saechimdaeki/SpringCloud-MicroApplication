812e2f753301929c9ef1d12fcfd28d69e2fb86df
saechimdaeki <anima94@kakao.com>
2021-05-16 15:19:48 +0900

    feat: loadUserByUsername()

--- UserMicroserviceProject/src/main/java/com/example/usermicroserviceproject/security/WebSecurity.java
+++ UserMicroserviceProject/src/main/java/com/example/usermicroserviceproject/security/WebSecurity.java
@@ -1,19 +1,49 @@
 package com.example.usermicroserviceproject.security;
 
+import com.example.usermicroserviceproject.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final Environment env;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {  //권한
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+//        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("192.168.200.167") //통과시키고 싶은 IP
+                .and()
+                .addFilter(getAuthenticationFilter());
 
         http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
+    }
+
+
+    // select pwd from users where email = ?
+    // db.pwd(encrypted)== input_pwd(encrypted)
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception { //인증
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
