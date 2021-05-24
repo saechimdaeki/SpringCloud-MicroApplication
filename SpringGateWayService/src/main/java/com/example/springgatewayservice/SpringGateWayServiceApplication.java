1278a21897af2f2fdb49319b4768c6d40259bd57
saechimdaeki <anima94@kakao.com>
2021-05-24 19:46:46 +0900

    feat: Spring CloudGateway add Spring Cloud Config

--- SpringGateWayService/src/main/java/com/example/springgatewayservice/SpringGateWayServiceApplication.java
+++ SpringGateWayService/src/main/java/com/example/springgatewayservice/SpringGateWayServiceApplication.java
@@ -1,13 +1,21 @@
 package com.example.springgatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringGateWayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringGateWayServiceApplication.class, args);
+    }
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository(){
+        return new InMemoryHttpTraceRepository();
     }
 
 }
